176ebac5e8edb8c5187b188b959250fee733c123
MSanjaySharma <sanjay98.sharma@gmail.com>
2020-10-27 09:00:51 +0530

    Update Error handling

--- src/lambda/token-gen.js
+++ src/lambda/token-gen.js
@@ -3,18 +3,27 @@ import { createPlatformEndpoint } from "../utils/sns-notif";
 export const handler = async (event) => {
   //throw new Error("range");
   const { deviceId, deviceToken } = JSON.parse(event.body);
-  const PlatformEndpoint = await createPlatformEndpoint(
-    "android",
-    deviceId,
-    deviceToken
-  );
-  console.log(`PlatformEndPoint-> ${JSON.stringify(PlatformEndpoint)}`);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Registered Succesfully. PlatformEndPoint-> ${JSON.stringify(
-        PlatformEndpoint
-      )}`,
-    }),
-  };
+  try {
+    const PlatformEndpoint = await createPlatformEndpoint(
+      "android",
+      deviceId,
+      deviceToken
+    );
+    console.log(`PlatformEndPoint-> ${JSON.stringify(PlatformEndpoint)}`);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Registered Succesfully. PlatformEndPoint-> ${JSON.stringify(
+          PlatformEndpoint
+        )}`,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: `Registered Failed. Error ${JSON.stringify(error)}}`,
+      }),
+    };
+  }
 };
