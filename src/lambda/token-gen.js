aab36b8316d46436b0f6df5e9f16fb07cba75b80
MSanjaySharma <sanjay98.sharma@gmail.com>
2020-10-27 10:37:45 +0000

    Added Publish-Message-test

--- src/lambda/token-gen.js
+++ src/lambda/token-gen.js
@@ -2,9 +2,17 @@ import { createPlatformEndpoint } from "../utils/sns-notif";
 
 export const handler = async (event) => {
   //throw new Error("range");
-  const { name } = event.body;
+  const { deviceId, deviceToken } = event.body;
+  const PlatformEndpoint = await createPlatformEndpoint(
+    "android",
+    deviceId,
+    deviceToken
+  );
+  console.log(`PlatformEndPoint-> ${PlatformEndpoint}`);
   return {
     statusCode: 200,
-    body: JSON.stringify({ range: `Hello World!!! ${name}` }),
+    body: JSON.stringify({
+      message: `Registered Succesfully. PlatformEndPoint-> ${PlatformEndpoint}`,
+    }),
   };
 };
